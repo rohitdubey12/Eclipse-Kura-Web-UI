2ccc89395ff0c6f35fe18cd932b7f96ea17b675c
rohitdubey12 <rohit@mba.cmd.eurotech.local>
2015-06-15 18:03:33 -0400

    RPC With UiBinder working (TODO DataModels)
    Replaced code in Default GWT Greeting service, since creating RPC from
    start wasn’t working due to some reason

--- src/org/eclipse/kura/skeleton4/client/Skeleton4.java
+++ src/org/eclipse/kura/skeleton4/client/Skeleton4.java
@@ -8,21 +8,9 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Skeleton4 implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting
-	 * service.
-	 */
 
 	private final Skeleton4Ui binder = GWT.create(Skeleton4Ui.class);
 
-	/**
-	 * This is the entry point method.
-	 */
 	@Override
 	public void onModuleLoad() {
 
--- src/org/eclipse/kura/skeleton4/client/Skeleton4Ui.java
+++ src/org/eclipse/kura/skeleton4/client/Skeleton4Ui.java
@@ -2,16 +2,16 @@ package org.eclipse.kura.skeleton4.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class Skeleton4Ui extends Composite {
+
+	@UiField
+	Label footerRight;
 
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
@@ -19,50 +19,21 @@ public class Skeleton4Ui extends Composite {
 	private static Skeleton4UIUiBinder uiBinder = GWT
 			.create(Skeleton4UIUiBinder.class);
 
-	// GWT.create(SystemPropertyService.class);
-
 	interface Skeleton4UIUiBinder extends UiBinder<Widget, Skeleton4Ui> {
 	}
 
 	public Skeleton4Ui() {
 		initWidget(uiBinder.createAndBindUi(this));
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogBox.setWidget(dialogVPanel);
-
 		greetingService.greetServer(new AsyncCallback<String>() {
 			@Override
 			public void onFailure(Throwable caught) {
 				// Show the RPC error message to the user
-				dialogBox.setText("Remote Procedure Call - Failure");
-				serverResponseLabel.addStyleName("serverResponseLabelError");
-				serverResponseLabel.setHTML("ERROR");
-				dialogBox.center();
-				closeButton.setFocus(true);
 			}
 
 			@Override
 			public void onSuccess(String result) {
-				dialogBox.setText("Remote Procedure Call");
-				serverResponseLabel.removeStyleName("serverResponseLabelError");
-				serverResponseLabel.setHTML(result);
-				dialogBox.center();
-				closeButton.setFocus(true);
+				footerRight.setText(result);
 			}
 		});
 
